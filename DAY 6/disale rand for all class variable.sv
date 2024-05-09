6146c0b53efef45ff4ab0e1e863dd277bc8b7fe5
kaviyacv1 <166712218+kaviyacv1@users.noreply.github.com>
2024-05-09 22:23:49 +0530

    Create disale rand for all class variable.sv

---
+++ DAY 6/disale rand for all class variable.sv
@@ -0,0 +1,21 @@
+class packet;
+  rand byte addr;
+  rand byte data;   
+endclass
+
+module rand_methods;
+  initial begin
+    packet pkt;
+    pkt = new();
+    
+    $display("\taddr.rand_mode() = %0d \t data.rand_mode() = %0d",pkt.addr.rand_mode(),pkt.data.rand_mode());
+    
+    pkt.rand_mode(0);
+    
+    pkt.randomize();
+    
+    $display("\taddr = %0d \t data = %0d",pkt.addr,pkt.data);
+    
+    $display("\taddr.rand_mode() = %0d \t data.rand_mode() = %0d",pkt.addr.rand_mode(),pkt.data.rand_mode());
+  end
+endmodule
