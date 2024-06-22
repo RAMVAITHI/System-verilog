4ac2b147a263b8619d0c87c7f3abc58fe9da66e5
kaviyacv1 <166712218+kaviyacv1@users.noreply.github.com>
2024-06-22 23:43:33 +0530

    Create q7.sv

---
+++ ASSIGNMENT-4/q7.sv
@@ -0,0 +1,27 @@
+module test();
+
+task dummy_task (input int x, string str);
+fork
+begin
+#x;
+end
+begin
+#10;
+end
+join_any
+disable fork;
+$display("%s is disabled at t =%0t", str, $time);
+endtask
+
+initial
+fork
+begin
+#2;
+dummy_task(5, "call 1");
+end
+begin
+#1;
+dummy_task(15, "call_2");
+end
+join
+endmodule
