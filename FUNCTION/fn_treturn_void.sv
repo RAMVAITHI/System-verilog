74ff6d7fb0b26a66ba40945b8bbc20cc0ed6e828
kaviyacv1 <166712218+kaviyacv1@users.noreply.github.com>
2024-06-26 07:03:59 +0530

    Create fn_treturn_void.sv

---
+++ FUNCTION/fn_treturn_void.sv
@@ -0,0 +1,12 @@
+module func_return_void;
+  
+  initial
+  begin
+    disp("\t passing string to function for displaying");
+    disp("\t passing string to function for displaying");
+  end
+
+    $display("%s",str);
+  endfunction : disp
+
+endmodule : func_return_void
