f8f696b234b6b49a75b1e137c61287099cadc52e
kaviyacv1 <166712218+kaviyacv1@users.noreply.github.com>
2024-06-26 06:50:06 +0530

    Create union_unpack.sv

---
+++ STRUCTURES AND UNION/union_unpack.sv
@@ -0,0 +1,21 @@
+module union1 ();
+  union {
+        int x;
+        byte y;
+    } data;
+
+    initial begin
+
+        data.x = 'hABCF10CD;
+
+        $display("\n x = %0h", data.x );
+        $display("\n y = %0h", data.y );
+
+        data.y = 'h56;
+
+        $display("\n x = %0h", data.x );
+        $display("\n y = %0h", data.y );
+        $display("\n data = %p", data);
+        $display("\n size of unpacked union :", $bits(data));
+    end
+endmodule
