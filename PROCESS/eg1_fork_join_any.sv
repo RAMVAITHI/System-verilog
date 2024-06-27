9b75fd95d274a44d00ef9440dd7c28145e62f631
kaviyacv1 <166712218+kaviyacv1@users.noreply.github.com>
2024-06-27 20:21:19 +0530

    Create eg1_fork_join_any.sv

---
+++ PROCESS/eg1_fork_join_any.sv
@@ -0,0 +1,19 @@
+module eg;
+  initial begin
+    #10 $display($time,"a");
+fork
+  begin
+    #1 $display($time,"b");
+  end
+  begin
+    #8 $display($time,"c");
+    #6 $display($time,"d");
+  end
+  begin
+    #5 $display($time,"e");
+    #10 $display($time,"f");
+  end
+join_any
+    $display($time,"g"); 
+  end
+endmodule
