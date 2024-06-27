9628a4931636dc8783169643fa190f0a03ca35d8
kaviyacv1 <166712218+kaviyacv1@users.noreply.github.com>
2024-06-27 21:28:23 +0530

    Create with_wait.sv

---
+++ PROCESS/DIFF BETWN WAIT AND DISABLE/with_wait.sv
@@ -0,0 +1,14 @@
+module eg;
+  initial begin
+    #10 $display($time,"a");
+fork
+  
+  #20 $display($time,"b");
+  #10 $display($time,"c");
+  #5 $display($time,"d");
+    
+join_any
+    wait fork;
+    $display($time,"e"); 
+  end
+endmodule
