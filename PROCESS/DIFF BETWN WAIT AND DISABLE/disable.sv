8bd87c5ba739f096f3ba70506db9c936fbda8fa7
kaviyacv1 <166712218+kaviyacv1@users.noreply.github.com>
2024-06-27 21:36:38 +0530

    Create disable.sv

---
+++ PROCESS/DIFF BETWN WAIT AND DISABLE/disable.sv
@@ -0,0 +1,16 @@
+module eg;
+  initial begin
+    #10 $display($time,"a");
+fork
+  
+  #20 $display($time,"b");
+  #10 $display($time,"c");
+  #5 $display($time,"d");
+    
+join_any
+ disable fork;
+  
+    $display($time,"e"); 
+  end
+endmodule
+
