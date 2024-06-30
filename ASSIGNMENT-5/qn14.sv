12259abe174fae2b1e9c11fa086c92eb8754a6fd
kaviyacv1 <166712218+kaviyacv1@users.noreply.github.com>
2024-06-30 15:51:46 +0530

    Create qn14.sv

---
+++ ASSIGNMENT-5/qn14.sv
@@ -0,0 +1,21 @@
+class trans;
+  int i;
+  function new();
+    i = 10;
+  endfunction
+endclass
+
+class ex_trans1 extends trans;
+  int i;
+  function new();
+    i = 20;
+  endfunction
+endclass
+
+ex_trans1 e_h;
+module abc();
+  initial begin
+    e_h = new();
+    $display("%p", e_h);
+  end
+endmodule
