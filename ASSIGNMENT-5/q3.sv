34857f125bfa8225c9d45fbf6ec97aae8083af88
kaviyacv1 <166712218+kaviyacv1@users.noreply.github.com>
2024-06-30 15:59:08 +0530

    Create q3.sv

---
+++ ASSIGNMENT-5/q3.sv
@@ -0,0 +1,17 @@
+class base;
+  int i;
+  static function get();
+    static int a;
+    a++;
+    $display(a);
+  endfunction
+endclass
+
+base b1_h, b2_h;
+module test();
+  initial begin
+    b1_h.get();
+    b1_h.get();
+    b2_h.get();
+  end
+endmodule
